89ad3081d08b563b3f9f8594ead1b0ec7cbb9970
Chinmay Pendharkar <chinmay@subnero.com>
2024-12-02 13:57:17 +0800

    feat(*): making jhwbus only build for Linux/ARM; supporting Docker based builds

--- src/main/c/i2c.c
+++ src/main/c/i2c.c
@@ -48,87 +48,52 @@ void log_info(const char *format, ...) {
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2COpen(JNIEnv* env, jobject obj, jstring dev, jint addr){
-    int fd = 42;
     const char* devName = (*env)->GetStringUTFChars(env, dev, NULL) ;
     log_open("jhwbus-i2c.txt");
     log_info("Opening I2C device %s", devName);
-#ifdef __linux__
-    fd = open(devName, O_RDWR);
-#endif
-    return fd;
+    return open(devName, O_RDWR);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CSetAddr(JNIEnv* env, jobject obj, jint fd, jbyte addr){
     log_info("Setting I2C addr 0x%02X", addr);
-    int rv = 0;
-    #ifdef __linux__
-        rv = ioctl(fd, I2C_SLAVE, addr);
-    #endif
-    return rv;
+    return ioctl(fd, I2C_SLAVE, addr);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CReadByte(JNIEnv* env, jobject obj, jint fd){
-    int rv = 0;
     log_info("Reading a byte");
-    #ifdef __linux__
-        rv = i2c_smbus_read_byte(fd);
-    #endif
-    return rv;
+    return i2c_smbus_read_byte(fd);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CWriteByte(JNIEnv* env, jobject obj, jint fd, jbyte data){
-    int rv = 0;
     log_info("Writing a byte 0x%02X", data);
-    #ifdef __linux__
-        rv = i2c_smbus_write_byte(fd, data);
-    #endif
-    return rv;
+    return i2c_smbus_write_byte(fd, data);;
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CWriteByteData(JNIEnv* env, jobject obj, jint fd, jbyte cmd, jbyte data){
-    int rv = 0;
     log_info("Writing a command 0x%02X and byte data 0x%02X", cmd, data);
-    #ifdef __linux__
-        rv = i2c_smbus_write_byte_data(fd, cmd, data);
-    #endif
-    return rv;
+    return i2c_smbus_write_byte_data(fd, cmd, data);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CWriteWordData(JNIEnv* env, jobject obj, jint fd, jbyte cmd, jint data){
-    int rv = 0;
     log_info("Writing a command 0x%02X and word data 0x%04X", cmd, data);
-    #ifdef __linux__
-        rv = i2c_smbus_write_word_data(fd, cmd, data);
-    #endif
-    return rv;
+    return i2c_smbus_write_word_data(fd, cmd, data);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CReadByteData(JNIEnv* env, jobject obj, jint fd, jbyte cmd){
-    int rv = 0;
     log_info("Reading a byte data with the command 0x%02X", cmd);
-    #ifdef __linux__
-        rv = i2c_smbus_read_byte_data(fd, cmd);
-    #endif
-    return rv;
+    return i2c_smbus_read_byte_data(fd, cmd);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CReadWordData(JNIEnv* env, jobject obj, jint fd, jbyte cmd){
-    int rv = 0;
     log_info("Reading a word data with the command 0x%02X", cmd);
-    #ifdef __linux__
-        rv = i2c_smbus_read_word_data(fd, cmd);
-    #endif
-    return rv;
+    return i2c_smbus_read_word_data(fd, cmd);
 }
 
 int Java_org_arl_jhwbus_I2CDevice_I2CReadBlockData(JNIEnv* env, jobject obj, jint fd, jbyte cmd, jbyteArray arr){
     jsize len = (*env)->GetArrayLength(env, arr);
     jbyte *rbuf = (*env)->GetByteArrayElements(env, arr, 0);
-    int rv = 0;
     log_info("Reading up to %d bytes of data with the command 0x%02X", len, cmd);
-    #ifdef __linux__
-        rv = i2c_smbus_read_i2c_block_data(fd, cmd, len, (unsigned char *)rbuf);
-    #endif
+    int rv = i2c_smbus_read_i2c_block_data(fd, cmd, len, (unsigned char *)rbuf);
     (*env)->ReleaseByteArrayElements(env, arr, rbuf, 0);
     return rv;
 }
@@ -137,10 +102,7 @@ int Java_org_arl_jhwbus_I2CDevice_I2CRead(JNIEnv* env, jobject obj, jint fd, jby
     jsize len = (*env)->GetArrayLength(env, arr);
     jbyte *rbuf = (*env)->GetByteArrayElements(env, arr, 0);
     log_info("Reading %d bytes of data", len);
-    int rv = 0;
-    #ifdef __linux__
-        rv = read(fd, rbuf, len);
-    #endif
+    int rv = read(fd, rbuf, len);
     (*env)->ReleaseByteArrayElements(env, arr, rbuf, 0);
     return rv;
 }
@@ -149,10 +111,7 @@ int Java_org_arl_jhwbus_I2CDevice_I2CWrite(JNIEnv* env, jobject obj, jint fd, jb
     jsize len = (*env)->GetArrayLength(env, arr);
     jbyte *wbuf = (*env)->GetByteArrayElements(env, arr, 0);
     log_info("Writing %d bytes of data", len);
-    int rv = 0;
-    #ifdef __linux__
-        rv = write(fd, wbuf, len);
-    #endif
+    int rv = write(fd, wbuf, len);
     (*env)->ReleaseByteArrayElements(env, arr, wbuf, 0);
     return rv;
 }
@@ -163,12 +122,9 @@ int Java_org_arl_jhwbus_I2CDevice_I2CWriteRead(JNIEnv* env, jobject obj, jint fd
     jsize rlen = (*env)->GetArrayLength(env, rarr);
     jbyte *rbuf = (*env)->GetByteArrayElements(env, rarr, 0);
     log_info("Writing %d bytes of data, reading %d bytes of data", wlen, rlen);
-    int rv = 0;
-    #ifdef __linux__
-        rv = write(fd, wbuf, wlen);
-        if (rv == wlen) rv = read(fd, rbuf, rlen);
-        else rv = -1;
-    #endif
+    int rv = write(fd, wbuf, wlen);
+    if (rv == wlen) rv = read(fd, rbuf, rlen);
+    else rv = -1;
     (*env)->ReleaseByteArrayElements(env, warr, wbuf, 0);
     (*env)->ReleaseByteArrayElements(env, rarr, rbuf, 0);
     return rv;
@@ -176,7 +132,5 @@ int Java_org_arl_jhwbus_I2CDevice_I2CWriteRead(JNIEnv* env, jobject obj, jint fd
 
 void Java_org_arl_jhwbus_I2CDevice_I2CClose(JNIEnv* env, jobject obj, jint fd){
     log_info("Closing the I2C Interface");
-    #ifdef __linux__
-        close(fd);
-    #endif
+    close(fd);
 }
